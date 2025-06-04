706f033284a10d59172ba4b306925a06cabe059d
Felipe Gutierrez Romero <felipegutiromero@gmail.com>
2025-06-04 11:08:53 -0500

    chore(app/layout): Imports of components for Ant Design added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,14 @@
 import 'antd/dist/reset.css';
 import "./globals.css";
+import { Layout, Avatar } from 'antd';
+import {
+  MailOutlined,
+  EyeOutlined,
+  RocketOutlined,
+  SettingOutlined,
+} from '@ant-design/icons';
+
+const { Sider, Content } = Layout;
 
 export default function RootLayout({
   children,
